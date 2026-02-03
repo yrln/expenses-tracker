c0075861740e1ceab31ce13421007baa4d66aa19
amadeus <doctermath@gmail.com>
2026-02-04 00:15:24 +0700

    prepare the project structure

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,11 @@
 package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"status": "ok"})
+	})
+}
